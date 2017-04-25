6e3bcc84365c38ba1e8c69161a5f29911a9aed83
Ari Hitron <ari.hitron@gmail.com>
2017-04-25 07:50:47 -0400

    update general reducers

--- src/reducers/general.js
+++ src/reducers/general.js
@@ -1,31 +1,30 @@
-
 const INITIAL = {
-    name: {
-        first: '',
-        last: ''
-    },
-    certified: false
+  name: {
+    first: '',
+    last: ''
+  },
+  certified: false
 };
 
 const reducerName = (state = INITIAL.name, action) => {
-    switch(action.type) {
+  switch(action.type) {
     case GEN_SET_FIRST:
-        return { ...state, first: action.payload };
+      return { ...state, first: action.payload };
     case GEN_SET_LAST:
-        return { ...state, last: action.payload };
+      return { ...state, last: action.payload };
     default:
-        return state;
-    }
+      return state;
+  }
 }
 
 export default reducerGeneral = (state = INITIAL, action) => {
-    switch(action.type) {
-        case GEN_SET_FIRST:
-        case GEN_SET_LAST:
-            return { ...state, name: reducerName(state.name, action) };
-        case GEN_SET_CERTIFIED:
-            return { ...state, certified: action.payload };
+  switch(action.type) {
+    case GEN_SET_FIRST:
+    case GEN_SET_LAST:
+      return { ...state, name: reducerName(state.name, action) };
+    case GEN_SET_CERTIFIED:
+      return { ...state, certified: action.payload };
         default:
-            return state;
-    }
+      return state;
+  }
 }
