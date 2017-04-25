4f96894484f8b647cbb5be8eb2750a92ca4dd89d
Ari Hitron <ari.hitron@gmail.com>
2017-04-25 07:40:42 -0400

    add general reducers

---
+++ src/reducers/general.js
@@ -0,0 +1,31 @@
+
+const INITIAL = {
+    name: {
+        first: '',
+        last: ''
+    },
+    certified: false
+};
+
+const reducerName = (state = INITIAL.name, action) => {
+    switch(action.type) {
+    case GEN_SET_FIRST:
+        return { ...state, first: action.payload };
+    case GEN_SET_LAST:
+        return { ...state, last: action.payload };
+    default:
+        return state;
+    }
+}
+
+export default reducerGeneral = (state = INITIAL, action) => {
+    switch(action.type) {
+        case GEN_SET_FIRST:
+        case GEN_SET_LAST:
+            return { ...state, name: reducerName(state.name, action) };
+        case GEN_SET_CERTIFIED:
+            return { ...state, certified: action.payload };
+        default:
+            return state;
+    }
+}
